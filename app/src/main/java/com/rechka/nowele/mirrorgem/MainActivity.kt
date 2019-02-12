5400f27eec86f62239f0150603a99f1059707ea0
Nowele Rechka <nrechka8788@eagle.fgcu.edu>
2019-02-11 21:39:01 -0500

    setup media player

--- app/src/main/java/com/rechka/nowele/mirrorgem/MainActivity.kt
+++ app/src/main/java/com/rechka/nowele/mirrorgem/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rechka.nowele.mirrorgem
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -9,15 +10,47 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var soundRecyclerView: RecyclerView
 
+    lateinit var mediaPlayer: MediaPlayer
+
+    var wasPlaying = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.theme_song)
+
         soundRecyclerView = findViewById(R.id.sound_recycler_view)
         soundRecyclerView.layoutManager = LinearLayoutManager(this)
-        soundRecyclerView.adapter = SoundBoardRecyclerViewAdapter(resources.getStringArray(R.array.buttons))
-
+        soundRecyclerView.adapter = SoundBoardRecyclerViewAdapter(this, mediaPlayer, resources.getStringArray(R.array.buttons))
     }
+
+    override fun onPause() {
+        super.onPause()
+        backgroundPlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        foregroundPlayer()
+    }
+
+    private fun backgroundPlayer() {
+        if (mediaPlayer.isPlaying) {
+            wasPlaying = true
+            mediaPlayer.pause()
+        } else {
+            wasPlaying = false
+        }
+    }
+
+    private fun foregroundPlayer() {
+        if (wasPlaying) {
+            mediaPlayer.start()
+        }
+    }
+
+
 }
